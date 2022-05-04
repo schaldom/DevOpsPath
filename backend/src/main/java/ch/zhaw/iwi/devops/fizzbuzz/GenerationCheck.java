696e8aa543aad0d0e197aad354ea9eff7568a1bb
Peacekeeper1337 <92219670+Peacekeeper1337@users.noreply.github.com>
2022-05-04 15:14:07 +0200

    Ergänzung Klasse GenerationCheck inkl. UnitTests GenerationCheckTest

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationCheck.java
@@ -0,0 +1,21 @@
+package ch.zhaw.iwi.devops.fizzbuzz;
+
+public class GenerationCheck {
+    
+    public String GenerationCheck(int jahrgang){
+        if (jahrgang >= 2011 && jahrgang <= 2025){
+            return "Generation Alpha";
+        } else if (jahrgang >= 1997 && jahrgang <= 2010){
+            return "Generation Z";
+        } else if (jahrgang >= 1981 && jahrgang <= 1996){
+            return "Generation Y";
+        } else if (jahrgang >= 1965 && jahrgang <= 1980){
+            return "Generation X";
+        } else if (jahrgang >= 1946 && jahrgang <= 1964){
+            return "Generation Boomer";
+        } else if (jahrgang >= 1928 && jahrgang <= 1945){
+            return "Generation Silent";
+        }          
+        return "undefinierbar";
+    }
+}
---
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationCheckTest.java
@@ -0,0 +1,51 @@
+package ch.zhaw.iwi.devops.fizzbuzz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GenerationCheckTest {
+
+    @Test
+
+    public void GenerationCheckAlpha(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("Generation Alpha", ac.GenerationCheck(2011));
+    }
+
+    @Test
+
+    public void GenerationCheckZ(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("Generation Z", ac.GenerationCheck(2000));
+    }
+    @Test
+
+    public void GenerationCheckY(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("Generation Y", ac.GenerationCheck(1982));
+    }
+    @Test
+
+    public void GenerationCheckX(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("Generation X", ac.GenerationCheck(1967));
+    }
+    @Test
+
+    public void GenerationCheckBoomer(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("Generation Boomer", ac.GenerationCheck(1950));
+    }
+    @Test
+
+    public void GenerationCheckSilent(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("Generation Silent", ac.GenerationCheck(1930));
+    }
+    @Test
+
+    public void GenerationCheckUndefiniert(){
+        GenerationCheck ac = new GenerationCheck();
+        Assert.assertEquals("undefinierbar", ac.GenerationCheck(10));
+    }
+}
