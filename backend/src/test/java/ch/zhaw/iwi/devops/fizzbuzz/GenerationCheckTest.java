9a0c5d17ba84ad88619ac6f3a5e047d986f452fb
Dominik Schallmeiner <schaldom@students.zhaw.ch>
2022-05-06 18:57:07 +0200

    Made an extra spelling mistake for JaCoCo & Jenkins

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationCheckTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/GenerationCheckTest.java
@@ -7,44 +7,42 @@ import org.junit.Test;
 public class GenerationCheckTest {
 
     @Test
-
     public void GenerationCheckAlpha(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("Generation Alpha", ac.GenerationCheck(2011));
     }
 
     @Test
-
     public void GenerationCheckZ(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("Generation Z", ac.GenerationCheck(2000));
     }
-    @Ignore
 
+    @Ignore
     public void GenerationCheckY(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("Generation Y", ac.GenerationCheck(1982));
     }
-    @Ignore
 
+    @Ignore
     public void GenerationCheckX(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("Generation X", ac.GenerationCheck(1967));
     }
-    @Test
 
+    @Test
     public void GenerationCheckBoomer(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("Generation Boomer", ac.GenerationCheck(1950));
     }
-    @Test
 
+    @Test
     public void GenerationCheckSilent(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("Generation Silent", ac.GenerationCheck(1930));
     }
-    @Test
 
+    @Test
     public void GenerationCheckUndefiniert(){
         GenerationCheck ac = new GenerationCheck();
         Assert.assertEquals("undefinierbar", ac.GenerationCheck(10));
